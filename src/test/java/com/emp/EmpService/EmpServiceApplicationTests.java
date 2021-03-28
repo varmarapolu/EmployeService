588ef675ee5e8fb6b1891f58fb1b188f3ea9c6ab
ravi <varmarapolu@gamil.com>
2021-03-28 17:41:06 +0530

    firstcommit

---
+++ src/test/java/com/emp/EmpService/EmpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.EmpService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
